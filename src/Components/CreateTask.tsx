1df1ed6b4365d19fee095f011c6dbc5d9349bcb2
OmAiR1Kh1 <abd.alkhahhar@gmail.com>
2023-11-18 17:19:26 +0200

    working on it

--- src/Components/CreateTask.tsx
+++ src/Components/CreateTask.tsx
@@ -1,1 +1,14 @@
-import React from 'react'
+import React from "react";
+interface Props {
+
+}
+
+const CreateTask: React.FC<Props> = () => {
+  return (
+    <>
+      <div className="task"></div>
+    </>
+  );
+};
+
+export default CreateTask;
